166d3ad3879dc4c66178efdf20f6fd9b266e9335
Ritchie <ritchieleeann@gmail.com>
2017-03-16 22:14:46 -0700

    get number from caller

--- run.py
+++ run.py
@@ -1,15 +1,27 @@
 from flask import Flask
-import twilio.twiml
+from twilio import twiml
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming requests."""
-    resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+def incoming_call():
+    """Handle incoming call and give instructions"""
+    response = twiml.Response()
+    response.say("Welcome to the Fizz Buzz hotline.", voice='woman')
+    with response.gather(action="/play_game", method="POST") as g:
+        g.say("Please enter a number and press pound to continue.", voice='woman')
 
-    return str(resp)
+    return str(response)
+
+@app.route("/play_game", methods=['GET', 'POST'])
+def play_game():
+    """Handle key press from player and respond"""
+
+    digit_pressed = request.values.get('Digits', None)
+    if digit_pressed == "1":
+        response = twiml.Response()
+        response.say("Your number was {}".format(digit_pressed), voice='woman')
+        return str(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
